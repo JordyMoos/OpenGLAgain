d4c2178862b4ae5caccdb8b1f8de31874c31a7bd
JordyMoos <jordymoos@gmail.com>
2020-02-01 12:38:36 +0100

    Pulse color

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <glad/glad.h>
-#include <GLFW\glfw3.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 
@@ -7,14 +7,15 @@ const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
 	"void main()\n"
 	"{\n"
-	"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"    gl_Position = vec4(aPos, 1.0);\n"
 	"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"uniform vec4 color;\n"
 	"void main()\n"
 	"{\n"
-	"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"    FragColor = color;\n"
 	"}\0";
 
 
@@ -145,7 +146,12 @@ int main()
 	
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int colorLocation = glGetUniformLocation(shaderProgram, "color");
 		glUseProgram(shaderProgram);
+		glUniform4f(colorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
