a66aaea3b53f44adcf9c43f642a028631b79cea9
JordyMoos <jordymoos@gmail.com>
2020-02-01 12:52:24 +0100

    Fancy color triangle

--- main.cpp
+++ main.cpp
@@ -5,17 +5,20 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 color;\n"
 	"void main()\n"
 	"{\n"
 	"    gl_Position = vec4(aPos, 1.0);\n"
+	"    color = aColor;\n"
 	"}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
-	"uniform vec4 color;\n"
+	"in vec3 color;\n"
 	"void main()\n"
 	"{\n"
-	"    FragColor = color;\n"
+	"    FragColor = vec4(color, 1.0);\n"
 	"}\0";
 
 
@@ -113,14 +116,13 @@ int main()
 
 	// setup vertices
 	float vertices[] = {
-		 0.5f,  0.5f, 0.0f, // top right
-		 0.5f, -0.5f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f, // top left
+		// positions			// colors
+		 0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	// bottom right
+		-0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	// bottom left
+		 0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f	// top
 	};
 	unsigned int indices[] = {
-		0, 1, 3, // first triangle
-		1, 2, 3  // second triangle
+		0, 1, 2, // first triangle
 	};
 
 	unsigned int VAO;
@@ -135,8 +137,10 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 	glBindVertexArray(0);
 
 	// main loop
@@ -149,6 +153,7 @@ int main()
 		float timeValue = glfwGetTime();
 		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
 		int colorLocation = glGetUniformLocation(shaderProgram, "color");
+
 		glUseProgram(shaderProgram);
 		glUniform4f(colorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
