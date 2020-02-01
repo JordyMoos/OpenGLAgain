4cf041724d15d3e9b0679a3a866c9245cadbd743
JordyMoos <jordymoos@gmail.com>
2020-02-01 22:34:17 +0100

    Added texture

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-#include "ShaderProgram.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "includes/stb_image.h"
+
+#include "src/ShaderProgram.h"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -44,25 +47,41 @@ int run()
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
+
+	// texture
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("./assets/image/container.jpg", &width, &height, &nrChannels, 0);
+	if (!data)
+	{
+		std::cout << "Error: could not load texture ./assets/image/container.jpg" << std::endl;
+		throw new std::runtime_error("Error: Could not load texture");
+	}
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
+
+
 	ShaderProgram shaderProgram = ShaderProgram("./src/shaders/vertex.vert", "./src/shaders/fragment.frag");
 	
 	// setup vertices
-	/*
 	float vertices[] = {
-		// positions			// colors			// texture
-		 0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	1.0f, 0.0f,	// bottom right
-		-0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f,	// bottom left
-		 0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f,	0.5f, 1.0f	// top
-	};
-	*/
-	float vertices[] = {
-		// positions			// colors			// texture
-		 0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	
-		-0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,
-		 0.0f,  0.5f, 0.0f,		0.0f, 0.0f, 1.0f
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 	unsigned int indices[] = {
 		0, 1, 2, // first triangle
+		2, 3, 0  // second triangle
 	};
 
 	unsigned int VAO;
@@ -77,10 +96,12 @@ int run()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 	glBindVertexArray(0);
 
 	// main loop
@@ -96,6 +117,7 @@ int run()
 		shaderProgram.use();
 		shaderProgram.set4f("color", 0.0f, greenValue, 0.0f, 1.0f);
 
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
--- src/shaders/fragment.frag
+++ src/shaders/fragment.frag
@@ -1,9 +1,13 @@
 #version 330 core
 
 out vec4 FragColor;
+
 in vec3 color;
+in vec2 textureCoordinate;
+
+uniform sampler2D aTexture;
 
 void main()
 {
-	FragColor = vec4(color, 1.0);
+	FragColor = texture(aTexture, textureCoordinate);
 }
